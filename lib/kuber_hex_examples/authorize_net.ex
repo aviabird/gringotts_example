8847b7fa4ea9131d170105c79912b0b683fcd0de
arjun289 <arjun289singh@gmail.com>
2017-12-14 18:16:15 +0530

    made changes to constants

--- lib/kuber_hex_examples/authorize_net.ex
+++ lib/kuber_hex_examples/authorize_net.ex
@@ -1,20 +1,33 @@
 defmodule Kuber.Hex.Examples.AuthorizeNet do
 
   alias Kuber.Hex.Gateways.AuthorizeNet
+  alias Kuber.Hex.CreditCard
   alias Kuber.Hex, as: Billing
+  
   @card %{
+    name: "Joe",
     number: "5424000000000015",
     expiration: "2020-12",
     cvc: "999"
   }
 
-  @opts [ refId: "123456", 
-    order: %{invoiceNumber: "INV-12345", description: "Product Description"}, 
-    lineItem: %{itemId: "1", name: "vase", description: "Cannes logo", quantity: "18", unitPrice: "45.00" }]
+  @opts [ refId: "123456",
+    order: %{invoiceNumber: "INV-12345",
+      description: "Product Description"
+      },
+    lineItem: %{itemId: "1",
+      name: "vase",
+      description: "Cannes logo",
+      quantity: "18",
+      unitPrice: "45.00"
+    }
+  ]
+  @amount 20.00
 
   def authorize() do
-    case Billing.authorize(:payment_worker,AuthorizeNet, 20.00, @card, @opts) do
-      {:ok, response} -> IO.inspect response
+    case Billing.authorize(:payment_worker,AuthorizeNet, @amount, @card, @opts) do
+      {:ok, response} -> response
+      {:error, response} -> response
     end
   end
 end
