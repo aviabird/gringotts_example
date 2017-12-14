9d58f33bf90430a5c9f473cbd8d01e846651ad9b
arjun289 <arjun289singh@gmail.com>
2017-12-14 20:43:20 +0530

    changed version to 1.5.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Gringotts.Examples.Mixfile do
     [
       app: :gringotts_examples,
       version: "0.1.0",
-      elixir: "~> 1.5.1",
+      elixir: "~> 1.5.2",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
